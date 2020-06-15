1d47b4d1ba25adf30c1756cb1d001bba421d3441
gnoeley <leonpelech@gmail.com>
2020-06-15 22:36:29 +0100

    add ability to configure client permissions using BAR_PERMISSIONS

--- entry.sh
+++ entry.sh
@@ -13,5 +13,25 @@ sed -i 's/password=.*/password="'"$BAR_PASSWORD"'"/' "${SETTINGS_XML}"
 sed -i 's/name=.*/name="'"$BAR_NAME"'"/' "${SETTINGS_XML}"
 sed -i 's/public=.*/public="true"/' "${SETTINGS_XML}"
 
+# Creat client Persmissions
+IFS=$","
+echo \
+'<?xml version="1.0" encoding="utf-8"?>
+<ClientPermissions>' \
+> clientpermissions.xml
+
+for permission in $BAR_PERMISSIONS
+do
+    IFS=$":"
+    arr=( $permission )
+    echo '<Client
+        name="'"${arr[0]}"'"
+        steamid="'"${arr[1]}"'"
+        preset="'"${arr[2]}"'" />' \
+    >> clientpermissions.xml
+done
+
+echo '</ClientPermissions>' >> clientpermissions.xml
+
 # Run the server!
 "${STEAMAPPDIR}/DedicatedServer"
