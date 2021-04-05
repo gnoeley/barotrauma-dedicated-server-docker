764fe87d2d64747ef5b3ee378475e58dfda7c82b
urmet0 <hannes.spenger@gmail.com>
2021-04-05 11:01:09 +0200

    Perms to actual needs & add ServerMessage

--- entry.sh
+++ entry.sh
@@ -13,11 +13,13 @@
 SETTINGS_XML=${STEAMAPPDIR}/serversettings.xml
 sed -i 's/password=.*/password="'"$BAR_PASSWORD"'"/' "${SETTINGS_XML}"
 sed -i 's/name=.*/name="'"$BAR_NAME"'"/' "${SETTINGS_XML}"
+sed -i 's/ServerMessage=.*/ServerMessage="'"$BAR_SERVERMESSAGE"'"/' "${SETTINGS_XML}"
 sed -i 's/public=.*/public="true"/' "${SETTINGS_XML}"
+
 
 # Creat client Persmissions
 CLIENT_PERMISSIONS_XML=${STEAMAPPDIR}/Data/clientpermissions.xml
-IFS=$","
+IFS=$";"
 echo \
 '<?xml version="1.0" encoding="utf-8"?>
 <ClientPermissions>' \
@@ -27,14 +29,29 @@ for permission in $BAR_PERMISSIONS
 do
     IFS=$":"
     arr=( $permission )
+    cmdstr=${arr[3]}
+    commands=""
+    clientend=""
+    if [ ! -z "$cmdstr" ];then
+      clientend=">"
+      IFS=","
+      read -a cmdarr <<< "$cmdstr"
+      for cmd in "${cmdarr[@]}"
+      do
+        commands="${commands} <command name=\"${cmd}\" />"
+      done
+    fi
     echo '<Client
         name="'"${arr[0]}"'"
         steamid="'"${arr[1]}"'"
-        preset="'"${arr[2]}"'" />' \
+        permissions="'"${arr[2]}"'"'"${clientend}"'
+        '"${commands}"'
+    </Client>' \
     >> $CLIENT_PERMISSIONS_XML
 done
 
 echo '</ClientPermissions>' >> $CLIENT_PERMISSIONS_XML
+
 
 # Run the server!
 "${STEAMAPPDIR}/DedicatedServer"
